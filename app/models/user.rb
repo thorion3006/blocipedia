606b69154a50f40cc563bc787c39d93a9be86c46
Sajeev Ramasamy <thorion3006@gmail.com>
2017-04-30 20:20:55 +0000

    User Roles: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :wikis, dependent: :destroy
+
+  enum role: [:admin, :standard, :premium]
+
+  after_initialize { self.role ||= :standard }
 end
