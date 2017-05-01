551b1b007acff0552b70f6f17e171fafc363b86e
Sajeev Ramasamy <thorion3006@gmail.com>
2017-05-01 12:09:40 +0000

    stripe integration

--- db/schema.rb
+++ db/schema.rb
@@ -33,10 +33,10 @@ ActiveRecord::Schema.define(version: 20170430201247) do
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "private"
+    t.boolean  "private",    default: false
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.index ["user_id"], name: "index_wikis_on_user_id"
   end
 
